580fca935a39b9a0c05f9059c7740c0b0dc383aa
tatianno <tferreiraalves@gmail.com>
2025-04-23 18:29:34 -0300

    Refactor chat processing logic to include while loop for continuous retrieval and finalization of chats

--- src/main.py
+++ src/main.py
@@ -7,8 +7,16 @@ provider = ChatBotProvider(
     base_url='https://api.gchat.app.br',
     access_token='67d84c27610880ae17cc3fb8'
 )
+
+request_executed = False
+chats = []
 controller = ChatController(provider)
-chats = controller.get_chats_without_response(hours=24, is_me=True)
-chats.extend(controller.get_chats_without_response(hours=24, is_me=False))
-result = controller.finalize_chats(chats)
-print(result)
+
+while len(chats) != 0 or request_executed == False:
+    chats = controller.get_chats_without_response(hours=24, is_me=True)
+    chats.extend(controller.get_chats_without_response(hours=24, is_me=False))
+    result = controller.finalize_chats(chats)
+    print(result)
+
+    if len(chats) == 0:
+        request_executed = True
