a10603369a73d4673758bc45e4d7de57cca51688
tatianno <tferreiraalves@gmail.com>
2025-04-23 17:49:07 -0300

    Update access token and adjust chat retrieval duration

--- src/main.py
+++ src/main.py
@@ -5,9 +5,10 @@ from providers.chatbot_provider import ChatBotProvider
 
 provider = ChatBotProvider(
     base_url='https://api.gchat.app.br',
-    access_token='663642c06f4ed0c2657819fc'
+    access_token='67d84c27610880ae17cc3fb8'
 )
 controller = ChatController(provider)
-chats = controller.get_chats_without_response(hours=8, is_me=True)
+chats = controller.get_chats_without_response(hours=24, is_me=True)
+chats.extend(controller.get_chats_without_response(hours=24, is_me=False))
 result = controller.finalize_chats(chats)
 print(result)
