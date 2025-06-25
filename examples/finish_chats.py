310aa0fb91b2ffd76e5e8b1d4f10cee8ef6bea5f
tatianno <tferreiraalves@gmail.com>
2025-06-25 12:42:48 -0300

    Atualizado script de exemplo

--- examples/finish_chats.py
+++ examples/finish_chats.py
@@ -7,9 +7,6 @@ load_dotenv()
 
 URL_API = os.environ.get('URL_API')
 TOKEN_API = os.environ.get('TOKEN_API')
-
-END_CHATS_WITH_ATTENDANTS_LAST_MESSAGE = True
-END_CHATS_WITH_CONTACTS_LAST_MESSAGE = True
 TIMEOUT = 2
 ALERT_TIME = 0.1
 ALERT_MESSAGE_TEXT = '''
@@ -28,8 +25,9 @@ controller = ChatController(provider, ALERT_MESSAGE_TEXT)
 data = controller.alert_chats(alert_time_in_hour=ALERT_TIME)
 print('send message alert', data)
 data = controller.finish_chats(
-    END_CHATS_WITH_ATTENDANTS_LAST_MESSAGE, 
-    END_CHATS_WITH_CONTACTS_LAST_MESSAGE, 
-    TIMEOUT
+    end_attendants_last_message=True,
+    end_contacts_last_message=False,
+    timeout=TIMEOUT,
+    discard_not_alert_message=True
 )
 print('closed chats', data)
